09d852e40cf7ae69032a3c934784234244ba6d9f
Lubomir Naydenov <lubonay@gmail.com>
2024-03-10 19:17:08 +0200

    Implement text to TextNode parsing

--- src/markdown_parser.py
+++ src/markdown_parser.py
@@ -1,4 +1,12 @@
-from textnode import TextNode, text_type_image, text_type_link
+from textnode import (
+    TextNode,
+    text_type_bold,
+    text_type_code,
+    text_type_image,
+    text_type_italic,
+    text_type_link,
+    text_type_text,
+)
 import re
 
 
@@ -90,3 +98,13 @@ def split_nodes_link(old_nodes):
         )
 
     return __split_or_default(old_nodes, split_links)
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
--- src/test_markdown_parser.py
+++ src/test_markdown_parser.py
@@ -15,6 +15,7 @@ from markdown_parser import (
     extract_markdown_links,
     split_nodes_image,
     split_nodes_link,
+    text_to_textnodes,
 )
 
 
@@ -214,6 +215,26 @@ class TestSplitLink(TestSplitBase):
                 TextNode("link", text_type_link, "https://another.link.com"),
                 TextNode(" and a ", text_type_text),
                 TextNode("trailing link", text_type_link, "https://link.com/trailing"),
+            ],
+        )
+
+
+class TestTextToTextNodes(TestSplitBase):
+    def test_text_to_textnodes(self):
+        text = "This is **text** with an *italic* word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        self.assert_split_result(
+            text_to_textnodes(text),
+            [
+                TextNode("This is ", text_type_text),
+                TextNode("text", text_type_bold),
+                TextNode(" with an ", text_type_text),
+                TextNode("italic", text_type_italic),
+                TextNode(" word and a ", text_type_text),
+                TextNode("code block", text_type_code),
+                TextNode(" and an ", text_type_text),
+                TextNode("image", text_type_image, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and a ", text_type_text),
+                TextNode("link", text_type_link, "https://boot.dev"),
             ],
         )
 
